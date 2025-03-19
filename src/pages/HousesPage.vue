5f0dc0e469335730bdcb63b0d9c3deba0d27c2c2
d-hooper <dallinhoop3@gmail.com>
2025-03-19 13:50:02 -0600

    added Houses Service and got house objects; outputting  house data to houses page

--- src/pages/HousesPage.vue
+++ src/pages/HousesPage.vue
@@ -1,4 +1,25 @@
 <script setup>
+import { AppState } from '@/AppState.js';
+import { housesService } from '@/services/HousesService.js';
+import { logger } from '@/utils/Logger.js';
+import { Pop } from '@/utils/Pop.js';
+import { computed, onMounted } from 'vue';
+
+const houses = computed(() => AppState.houses)
+
+onMounted(() => {
+  getHouses()
+})
+
+async function getHouses() {
+  try {
+    await housesService.getHouses()
+  }
+  catch (error) {
+    Pop.error('Could not get houses', error);
+    logger.error('COULD NOT GET HOUSES', error)
+  }
+}
 
 </script>
 
@@ -13,6 +34,11 @@
       </div>
     </div>
   </section>
+  <div class="container">
+    <div class="row">
+      {{ houses }}
+    </div>
+  </div>
 </template>
 
 
